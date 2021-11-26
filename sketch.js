c166cf953562f649dd83fcf64f624e9acee488a6
sanjeevankumar <94963339+sanjeevankumar@users.noreply.github.com>
2021-11-26 21:21:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var weight=[35,38,40,45,50]
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+var sum =weight[0]+ weight[1]+weight[2]+weight[3]+weight[4]
+var average = sum/weight.length
+console.log(average)
+
+}
+
+function draw() 
+{
+background(51);
+
+}
+
